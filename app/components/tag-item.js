b1df67e045a9af9e31c4e35399855fa55b862f98
Chris Manson <chris@manson.ie>
2019-03-28 10:03:12 +0000

    removing unused imports

--- app/components/tag-item.js
+++ app/components/tag-item.js
@@ -1,8 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { htmlSafe } from '@ember/string';
 import { computed } from '@ember/object';
-import { url } from 'ember-ghost-w3css-blog-template/helpers/url'
 
 export default Component.extend({
   blog: service(),
